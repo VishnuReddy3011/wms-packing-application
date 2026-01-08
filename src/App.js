4e30f38adbbd85e5f4c734a46e7e574471d87522
VishnuReddy3011 <sajjalavishnuvardhanreddy2001@gmail.com>
2026-01-08 23:19:52 +0530

    Error msg

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ function App() {
         );
       }
     } catch (err) {
-      setFailureMessage("Server not reachable");
+      setFailureMessage(err);
     } finally {
       setLoading(false);
     }
